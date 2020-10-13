18cc11cfd2462727f6338fc3a729a76f37b28ce9
msafonov <msafonovmail@gmail.com>
2020-10-13 10:05:15 +0300

    build: базовая конфигурация проекта

---
+++ src/main/java/com/github/mikesafonov/operatorbot/OperatorBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.mikesafonov.operatorbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperatorBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperatorBotApplication.class, args);
+	}
+
+}
