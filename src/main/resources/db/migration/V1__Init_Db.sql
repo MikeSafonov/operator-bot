226f894dc769e27d96c7bc642a8a454d9b9c0879
gagrim1 <gaggy.rom@gmail.com>
2020-10-16 13:00:12 +0300

    fix: правка миграции

---
+++ src/main/resources/db/migration/V1__Init_Db.sql
@@ -0,0 +1,19 @@
+create table internal_users(
+    id int primary key auto_increment,
+    telegram_id varchar(255) not null,
+    full_name varchar(255) not null,
+    status int not null
+) engine=InnoDB;
+
+create table external_users(
+    id int primary key auto_increment,
+    telegram_id varchar(255) not null,
+    full_name varchar(255) not null
+) engine=InnoDB;
+
+create table timetable(
+    id int primary key auto_increment,
+    user_id int not null,
+    times date UNIQUE not null,
+    FOREIGN KEY (user_id) REFERENCES internal_users(id)
+) engine=InnoDB;
