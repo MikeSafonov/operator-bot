36bee2552385a00985c4f5f6188ed3d2524bc8a2
gagrim1 <gaggy.rom@gmail.com>
2020-11-12 15:07:25 +0300

    fix: исправлена сущность Timetable

--- src/main/java/com/github/mikesafonov/operatorbot/model/Timetable.java
+++ src/main/java/com/github/mikesafonov/operatorbot/model/Timetable.java
@@ -1,6 +1,6 @@
 package com.github.mikesafonov.operatorbot.model;
 
-import java.sql.Date;
+import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -21,6 +21,25 @@ public class Timetable {
 	@JoinColumn(name = "user_id")
 	private InternalUser userId;
 	@Column(name = "times")
-	private Date time;
+	private LocalDate time;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public InternalUser getUserId() {
+		return userId;
+	}
+	public void setUserId(InternalUser userId) {
+		this.userId = userId;
+	}
+	public LocalDate getTime() {
+		return time;
+	}
+	public void setTime(LocalDate time) {
+		this.time = time;
+	}
 
 }
