d7829e69e66827b0ba569b319c87352ea2cd6d2c
yuromanov <gaggy.rom@gmail.com>
2021-01-21 13:07:53 +0300

    feat: реализовано оповещение о дежурстве

--- src/main/java/com/github/mikesafonov/operatorbot/MessageSender.java
+++ src/main/java/com/github/mikesafonov/operatorbot/MessageSender.java
@@ -1,12 +1,17 @@
 package com.github.mikesafonov.operatorbot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.DefaultAbsSender;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
+@Slf4j
 public class MessageSender extends DefaultAbsSender {
+
 	@Value("${bot.token}")
 	private String botToken;
 
@@ -19,4 +24,11 @@ public class MessageSender extends DefaultAbsSender {
 		return botToken;
 	}
 
+	public void sendMessage(SendMessage sendMessage) {
+		try {
+			execute(sendMessage);
+		} catch (TelegramApiException ex) {
+			log.error("Message didn't send. " + ex);
+		}
+	}
 }
