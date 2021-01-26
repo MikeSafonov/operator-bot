03ecce4230eacd855f7511196cf4b34cbbeee117
yuromanov <gaggy.rom@gmail.com>
2021-01-26 16:35:05 +0300

    refactor: сокращен ненужный код

--- src/main/java/com/github/mikesafonov/operatorbot/exceptions/UserNotFoundException.java
+++ src/main/java/com/github/mikesafonov/operatorbot/exceptions/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package com.github.mikesafonov.operatorbot.exceptions;
 
-public class UserNotFoundException extends Exception {
+public class UserNotFoundException extends RuntimeException {
 	public UserNotFoundException(String errorMessage) {
 		super(errorMessage);
 	}
